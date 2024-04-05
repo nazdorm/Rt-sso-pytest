c0a3ad4243e35509d1693fc05f90091a860fb06f
Ivan <114081852+nazdorm@users.noreply.github.com>
2024-04-05 16:33:41 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ B_URL = "https://b2c.passport.rt.ru/"
 VALID_EMAIL = "chistotest993@mail.ru"
 VALID_PASS = ("zzzaaa5153A") # мыло zzzaaa5153A!
 VALID_LOGIN = ("rtkid_1679572307602")
-VALID_NUMBER = ("79935327669")
+VALID_NUMBER = ("") #вирт.номер
 
 INVALID_NUMBER = ("71234567890")
 INVALID_EMAIL = ("mailer@mail.mail")
